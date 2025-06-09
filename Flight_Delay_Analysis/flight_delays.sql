b27e9584be777137eb0327b951144df1bd043988
Henry Smith <henry.nsmith12@gmail.com>
2025-06-09 14:55:39 -0500

    Update flight_delays.sql

--- Flight_Delay_Analysis/flight_delays.sql
+++ Flight_Delay_Analysis/flight_delays.sql
@@ -1,7 +1,7 @@
-/*The dataset used in the below analysis contained only late flights. For further late 
-flightexploration and greater detail, on-time flight data during the same time period should be 
-used in conjunction with the plane_data.csv data to identify percentages of late flights by 
-flight categories*/
+/*The dataset used in the below analysis contains only late flights. For further late flight
+exploration and greater detail, on-time flight data during the same time period should be used 
+in conjunction with the plane_data.csv data to identify percentages of late flights by flight 
+categories*/
 
 
 /*Percentage of Carrier and Late Aircraft 
